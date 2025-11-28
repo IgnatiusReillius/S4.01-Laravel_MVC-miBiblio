24f0a43c1775c821a4bbbc519d134c8d5c5e9982
IgnatiusReillius <47978271+IgnatiusReillius@users.noreply.github.com>
2025-11-28 19:00:46 +0100

    Revert "Feature sorting books"

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\BookUserController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,18 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class,'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('/bookUser', BookUserController::class);
-
-    Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
 });
 
 require __DIR__.'/auth.php';
